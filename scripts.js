ab70e9b8522a1853ad09789831222f2b2fd4754c
SharadhiPrabhu <146694704+SharadhiPrabhu@users.noreply.github.com>
2024-07-28 16:54:28 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector('.navbar');
+    const navbar = document.querySelector('.header');
+    const navLinks = document.querySelectorAll('.nav-item a');
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
-            navbar.style.background = 'linear-gradient(90deg, #283c86, #45a247)';
+            navbar.style.background = 'linear-gradient(90deg, #5a3e73, #8e44ad)';
         } else {
-            navbar.style.background = 'linear-gradient(90deg, #ff7e5f, #feb47b)';
+            navbar.style.background = 'linear-gradient(90deg, #8e44ad, #9b59b6)';
         }
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            link.style.backgroundColor = '#fff';
+            link.style.color = '#8e44ad';
+        });
+
+        link.addEventListener('mouseout', () => {
+            link.style.backgroundColor = '';
+            link.style.color = '';
+        });
     });
 });
